4ef2b71d637ae549348e79cac5dfdc2d1524ac5a
TheKunFo <thekunfo@icloud.com>
2025-07-09 12:08:13 -0500

    Refactor BASE_URL_BACKEND to use environment variables; clean up accessToken and accessApplication functions

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,18 +1,21 @@
 export const CLIENT_ID = "2fd69128dd2a402ea9dca8a5ea6b4d6f";
 export const CLIENT_SECRET = "81526518e36446eea552ef803ad36c3f";
-export const BASE_URL_BACKEND = "http://127.0.0.1:3001";
+export const BASE_URL_BACKEND =
+  process.env.NODE_ENV === "production"
+    ? "https://apispotify.thekunfo.com"
+    : "http://127.0.0.1:3001";
 export const accessToken = () => {
-    const token = localStorage.getItem('token')
-    if (!token) {
-        return null;
-    }
-    return token;
-}
+  const token = localStorage.getItem("token");
+  if (!token) {
+    return null;
+  }
+  return token;
+};
 
 export const accessApplication = () => {
-    const token = localStorage.getItem('app')
-    if (!token) {
-        return null;
-    }
-    return token;
-}
+  const token = localStorage.getItem("app");
+  if (!token) {
+    return null;
+  }
+  return token;
+};
